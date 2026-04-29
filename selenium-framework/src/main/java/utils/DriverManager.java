c280764699a706fd500a603273cd3a83db544b4f
barsa2607 <barsa200026@gmail.com>
2026-04-29 13:26:24 +0530

    Create DriverManager.java

---
+++ selenium-framework/src/main/java/utils/DriverManager.java
@@ -0,0 +1,31 @@
+package utils;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class DriverManager {
+
+    private static ThreadLocal<WebDriver> driver = new ThreadLocal<>();
+
+    public static WebDriver getDriver() {
+        if (driver.get() == null) {
+            WebDriverManager.chromedriver().setup();
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--headless"); // runs without opening browser
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+            driver.set(new ChromeDriver(options));
+            driver.get().manage().window().maximize();
+        }
+        return driver.get();
+    }
+
+    public static void quitDriver() {
+        if (driver.get() != null) {
+            driver.get().quit();
+            driver.remove();
+        }
+    }
+}
